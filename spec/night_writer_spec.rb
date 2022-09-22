ef203a0082ebfa635eb7f6d198de78e4875a444d
Eli Fuchsman <efuchsman@gmail.com>
2022-09-22 16:22:36 -0600

    Write method that passes test for reading file

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -3,22 +3,22 @@ require './lib/night_writer.rb'
 
 RSpec.describe NightWriter do
   it 'exists' do
-    night_writer = NightWriter.new('./text/message.txt')
+    night_writer = NightWriter.new('message.txt')
 
     expect(night_writer).to be_an_instance_of(NightWriter)
 
   end
 
   it "can has readable attributes" do
-    night_writer = NightWriter.new('./text/message.txt')
+    night_writer = NightWriter.new('message.txt')
 
-    expect(night_writer.filename).to eq('./text/message.txt').or(eq('./text/braille'))
+    expect(night_writer.filename).to eq('message.txt').or(eq('braille.txt'))
 
   end
 
   it "can take in readable input from a file" do
 
-    night_writer = NightWriter.new('./text/message.txt')
+    night_writer = NightWriter.new('message.txt')
 
     expect(night_writer.read_message).to eq("there's vomit on his sweater already mom's spaghetti"
     )
