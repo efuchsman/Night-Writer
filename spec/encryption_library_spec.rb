3129111549615defa28ee2ffcab25b5ce086a278
Eli Fuchsman <efuchsman@gmail.com>
2022-09-22 23:31:20 -0600

    Initialize EncryptionLibrary class and creat test for returning lowercase alphabet

---
+++ spec/encryption_library_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require './lib/encryption_library'
+
+RSpec.describe EncryptionLibrary do
+  it "exists" do
+    encryption_library = EncryptionLibrary.new
+
+    expect(encryption_library).to be_an_instance_of(EncryptionLibrary)
+  end
+
+  it "has a hash of the braille alphabet" do
+    encryption_library = EncryptionLibrary.new
+
+    expect(encryption_library.the_braille_alphabet).to eq(
+      {
+        'a' => ['0.', '..', '..'],
+        'b' => ['0.', '0.', '..'],
+        'c' => ['00', '..', '..'],
+        'd' => ['00', '.0', '..'],
+        'e' => ['0.', '.0', '..'],
+        'f' => ['00', '0.', '..'],
+        'g' => ['00', '00', '..'],
+        'h' => ['0.', '00', '..'],
+        'i' => ['.0', '0.', '..'],
+        'j' => ['.0', '00', '..'],
+        'k' => ['0.', '..', '0.'],
+        'l' => ['0.', '0.', '0.'],
+        'm' => ['00', '..', '0.'],
+        'n' => ['00', '.0', '0.'],
+        'o' => ['0.', '.0', '0.'],
+        'p' => ['00', '0.', '0.'],
+        'q' => ['00', '00', '0.'],
+        'r' => ['0.', '00', '0.'],
+        's' => ['.0', '0.', '0.'],
+        't' => ['.0', '00', '0.'],
+        'u' => ['0.', '..', '00'],
+        'v' => ['0.', '0.', '00'],
+        'w' => ['.0', '00', '.0'],
+        'x' => ['00', '..', '00'],
+        'y' => ['00', '.0', '00'],
+        'z' => ['0.', '.0', '00']
+                                  }
+                                    )
+  end
+end
