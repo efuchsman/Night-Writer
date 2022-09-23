c69613332f8942e4d430338533e0bfc5aec957c0
Eli Fuchsman <efuchsman@gmail.com>
2022-09-22 21:38:13 -0600

    Fix variable for counting characters

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,7 +3,7 @@ message = File.open(ARGV[0], "r")
 message_reader = message.read
 
 new_file = File.open(ARGV[1], "w")
-writer = "but on the surface he looks calm and ready"
-encrypter = new_file.write(writer)
 
-puts "Created #{ARGV[1]} containing #{writer.length} characters"
+encrypter = new_file.write(message_reader)
+
+puts "Created #{ARGV[1]} containing #{message_reader.length} characters"
