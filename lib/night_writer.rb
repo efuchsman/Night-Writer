773fdc03761f69c706a2bfce7c4615cbc9a543fb
Eli Fuchsman <efuchsman@gmail.com>
2022-09-22 18:32:38 -0600

    redesign night_writer file to transition away from a class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,15 +1,6 @@
-class NightWriter
-
-  attr_reader :filename
-
-  def initialize(filename = 'message.txt')
-    @filename = filename
-
-  end
-
-  def read_message
-    file = File.readlines(@filename, chomp: true, headers: true, header_converters: :symbol)
-     p file.join("','")[/\A"(.*)"\z/m,1]
-  end
-
-end
+message = File.open(ARGV[0], "r")
+message_reader = message.read
+message.close
+decrypter = File.open(ARGV[1], "w")
+decrypter.write("but on the surface he looks calm and ready")
+decrypter.close
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,27 +1,27 @@
-require 'rspec'
-require './lib/night_writer.rb'
+# require 'rspec'
+# require './lib/night_writer.rb'
 
-RSpec.describe NightWriter do
-  it 'exists' do
-    night_writer = NightWriter.new('message.txt')
+# RSpec.describe NightWriter do
+#   it 'exists' do
+#     night_writer = NightWriter.new('message.txt')
 
-    expect(night_writer).to be_an_instance_of(NightWriter)
+#     expect(night_writer).to be_an_instance_of(NightWriter)
 
-  end
+#   end
 
-  it "can has readable attributes" do
-    night_writer = NightWriter.new('message.txt')
+#   it "can has readable attributes" do
+#     night_writer = NightWriter.new('message.txt')
 
-    expect(night_writer.filename).to eq('message.txt').or(eq('braille.txt'))
+#     expect(night_writer.filename).to eq('message.txt').or(eq('braille.txt'))
 
-  end
+#   end
 
-  it "can take in readable input from a file" do
+#   it "can take in readable input from a file" do
 
-    night_writer = NightWriter.new('message.txt')
+#     night_writer = NightWriter.new('message.txt')
 
-    expect(night_writer.read_message).to eq("there's vomit on his sweater already mom's spaghetti"
-    )
-  end
+#     expect(night_writer.read_message).to eq("there's vomit on his sweater already mom's spaghetti"
+#     )
+#   end
 
-end
+# end
